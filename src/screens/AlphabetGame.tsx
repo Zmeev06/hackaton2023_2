615d4a64dfeb85cdca49582b372f9999191e3e9b
Zmeev06 <se.rg.ey06@icloud.com>
2023-04-09 01:40:46 +0300

    game

--- src/screens/AlphabetGame.tsx
+++ src/screens/AlphabetGame.tsx
@@ -1,38 +1,55 @@
-import {View, Text, SafeAreaView, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 import {globalStyles} from '../globalStyles';
 import {RouteProp, useNavigation} from '@react-navigation/native';
-import {colors} from '../variables/colors'
+import {colors} from '../variables/colors';
 import Header from '../components/Header';
-import { letters } from '../variables/letters';
-
+import {letters} from '../variables/letters';
 
 const AlphabetGame: React.FC = () => {
   const navigation = useNavigation();
+  const [letter, setLetter] = useState('')
   return (
-  <SafeAreaView style={globalStyles.safeAreaView}>
-    <Header title="Алфавит" color={colors.green_dark} />
-    <View></View>
-    <View style={styles.lettersBlock}>
-        {letters.map((item, index) => <TouchableOpacity style={styles.letterBlock} key={index}><Image style={styles.letterImg} source={item} /></TouchableOpacity>)}
-    </View>
-  </SafeAreaView>
-  )
+    <SafeAreaView style={globalStyles.safeAreaView}>
+      <Header title="Алфавит" color={colors.green_dark} />
+      <View style={styles.letterMain}>
+        <Image />
+      </View>
+      <View style={styles.lettersBlock}>
+        {letters.map((item, index) => (
+          <TouchableOpacity style={styles.letterBlock} key={index} onPress={() => setLetter(index)}>
+            <Image style={styles.letterImg} source={item} />
+          </TouchableOpacity>
+        ))}
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   lettersBlock: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   letterBlock: {
     width: 40,
     height: 40,
-
   },
   letterImg: {
     width: 30,
-    height: 30
+    height: 30,
+  },
+  letterMain: {
+    height: '70%'
   }
 });
 
