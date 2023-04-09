3218324548e0d291b9a850b86d4ce666a30ec8b4
wyylame <wyylame@yandex.ru>
2023-04-09 04:14:43 +0300

    move sounds

---
+++ src/data/alphabetSounds.ts
@@ -0,0 +1,69 @@
+import sound_1 from '../assets/sounds/alphabet/sound1.mp3';
+import sound_2 from '../assets/sounds/alphabet/sound2.mp3';
+import sound_3 from '../assets/sounds/alphabet/sound3.mp3';
+import sound_4 from '../assets/sounds/alphabet/sound4.mp3';
+import sound_5 from '../assets/sounds/alphabet/sound5.mp3';
+import sound_6 from '../assets/sounds/alphabet/sound6.mp3';
+import sound_7 from '../assets/sounds/alphabet/sound7.mp3';
+import sound_8 from '../assets/sounds/alphabet/sound8.mp3';
+import sound_9 from '../assets/sounds/alphabet/sound9.mp3';
+import sound_10 from '../assets/sounds/alphabet/sound10.mp3';
+import sound_11 from '../assets/sounds/alphabet/sound11.mp3';
+import sound_12 from '../assets/sounds/alphabet/sound12.mp3';
+import sound_13 from '../assets/sounds/alphabet/sound13.mp3';
+import sound_14 from '../assets/sounds/alphabet/sound14.mp3';
+import sound_15 from '../assets/sounds/alphabet/sound15.mp3';
+import sound_16 from '../assets/sounds/alphabet/sound16.mp3';
+import sound_17 from '../assets/sounds/alphabet/sound17.mp3';
+import sound_18 from '../assets/sounds/alphabet/sound18.mp3';
+import sound_19 from '../assets/sounds/alphabet/sound19.mp3';
+import sound_20 from '../assets/sounds/alphabet/sound20.mp3';
+import sound_21 from '../assets/sounds/alphabet/sound21.mp3';
+import sound_22 from '../assets/sounds/alphabet/sound22.mp3';
+import sound_23 from '../assets/sounds/alphabet/sound23.mp3';
+import sound_24 from '../assets/sounds/alphabet/sound24.mp3';
+import sound_25 from '../assets/sounds/alphabet/sound25.mp3';
+import sound_26 from '../assets/sounds/alphabet/sound26.mp3';
+import sound_27 from '../assets/sounds/alphabet/sound27.mp3';
+import sound_28 from '../assets/sounds/alphabet/sound28.mp3';
+import sound_29 from '../assets/sounds/alphabet/sound29.mp3';
+import sound_30 from '../assets/sounds/alphabet/sound30.mp3';
+import sound_31 from '../assets/sounds/alphabet/sound31.mp3';
+import sound_32 from '../assets/sounds/alphabet/sound32.mp3';
+import sound_33 from '../assets/sounds/alphabet/sound33.mp3';
+
+export const alphabetSounds = [
+  sound_1,
+  sound_2,
+  sound_3,
+  sound_4,
+  sound_5,
+  sound_6,
+  sound_7,
+  sound_8,
+  sound_9,
+  sound_10,
+  sound_11,
+  sound_12,
+  sound_13,
+  sound_14,
+  sound_15,
+  sound_16,
+  sound_17,
+  sound_18,
+  sound_19,
+  sound_20,
+  sound_21,
+  sound_22,
+  sound_23,
+  sound_24,
+  sound_25,
+  sound_26,
+  sound_27,
+  sound_28,
+  sound_29,
+  sound_30,
+  sound_31,
+  sound_32,
+  sound_33,
+];
